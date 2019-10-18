2acbef3f3dd8411950c62d6d4325df209fc9e412
unknown <gilc.alejandro@gmail.com>
2019-10-18 16:05:56 -0400

    HTML Template

--- ProblemSolverApp-SPA/src/app/home/home.component.ts
+++ ProblemSolverApp-SPA/src/app/home/home.component.ts
@@ -3,10 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css', '../../../src/assets/css/floating-labels.css']
 })
 export class HomeComponent implements OnInit {
-
+  title = 'app-SPA';
   constructor() { }
 
   ngOnInit() {
