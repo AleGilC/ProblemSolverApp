03f2c24056a934005de395db08b30bc53bad3eed
unknown <gilc.alejandro@gmail.com>
2019-10-21 16:09:29 -0400

    Working on login

--- ProblemSolverApp-SPA/src/app/register/register.component.ts
+++ ProblemSolverApp-SPA/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css', '../../../src/assets/css/floating-labels.css']
 })
 export class RegisterComponent implements OnInit {
+  model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Logged In Successfully');
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !! token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('Logged Out');
+  }
 }
--- ProblemSolverApp.API/Dtos/UseForLoginDto.cs
+++ ProblemSolverApp.API/Dtos/UseForLoginDto.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProblemSolverApp.API.Dtos
 {
     public class UseForLoginDto
     {
+        [Required]
         public string Username { get; set; }
-
+        [Required]
         public string Password { get; set; }
     }
 }
